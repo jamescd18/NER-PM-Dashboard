36141ee3b52ed83a806e2091338e2ee2ad5413ed
jamescd18 <51571627+jamescd18@users.noreply.github.com>
2020-07-19 22:42:10 -0700

    added work package lookup feature

--- Code.js
+++ Code.js
@@ -3,15 +3,41 @@ Document: Main JS code
 
 */
 
+var scriptProps = PropertiesService.getScriptProperties(); // Google apps script properties
 
+// doGet : HttpsRequest -> HTML
+// Serve up the website HTML from Index.html
 function doGet(request) {
     return HtmlService.createTemplateFromFile('Index').evaluate();
 }
 
+// include : String -> HTML
+// Serve HTML content from the file specified by the string file name
 function include(filename) {
     return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
 
+// testFunc : n/a -> Error
+// Function that just throws an error
 function testFunc() {
     throw "example error";
+}
+
+// getSheetData : String String -> Array[][]
+// Fetches 2D array of spreadsheet data given the script property name where the file ID is stored and the sheet name
+function getSheetData(fileIDPropName, sheetName) {
+    var fileId = scriptProps.getProperty(fileIDPropName);
+    var file = SpreadsheetApp.openById(fileId);
+    var sheet = file.getSheetByName(sheetName);
+    var range = sheet.getDataRange();
+    return range.getValues();
+}
+
+// validateWbsNum : String -> n/a
+// Is the provided string a valid Work Breakdown Structure number? (If no, throw an error)
+function validateWbsNum(wbsNum) {
+    var errorMsg = "WBS Invalid: ";
+    if (wbsNum.length != 9) {
+        throw errorMsg + "incorrect length";
+    }
 }
---
+++ Code_WorkPackages.js
@@ -0,0 +1,71 @@
+/*
+Document: JS code specific for Work Packages
+
+*/
+
+// getWorkPackage : String -> HTML
+// Produce HTML display output for given WBS #'s work package
+function getWorkPackage(wbsNum) {
+    return getWorkPackageHtml(getWorkPackageObj(wbsNum));
+}
+
+// getWorkPackageObj : String -> Object[Work Package]
+// Build work package object from spreadsheet data
+function getWorkPackageObj(wbsNum) {
+    validateWbsNum(wbsNum);
+    var data = getSheetData('mainSheetID', 'Work Packages');
+    var rowData = [];
+    for (var rowIdx = 1; rowIdx < data.length; rowIdx++) {
+        if (data[rowIdx][2] == wbsNum) {
+            rowData = data[rowIdx];
+            break;
+        }
+    }
+    if (rowData.length == 0) {
+        throw "No Work Package data found."
+    }
+    var workPackage = {
+        project: rowData[0],
+        lead: rowData[1],
+        wbsNum: rowData[2],
+        name: rowData[3],
+        duration: rowData[4] + " weeks",
+        budget: "$" + rowData[5],
+        dependancies: rowData[6],
+        deliverable: rowData[7],
+        description: rowData[8],
+        changes: rowData[9]
+    };
+    return workPackage;
+}
+
+// getWorkPackageHtml : Object[Work Package] -> HTML
+// Builds HTML description list from fields in given work package
+function getWorkPackageHtml(workPackage) {
+    var html = `<div class="data-frame">
+                    <dl class="row">
+                        <dt class="col-sm-3">Project</dt>
+                        <dd class="col-sm-9">` + workPackage.project + `</dd>
+                        <dt class="col-sm-3">Project Lead</dt>
+                        <dd class="col-sm-9">` + workPackage.lead + `</dd>
+                        <hr>
+                        <dt class="col-sm-3">WBS #</dt>
+                        <dd class="col-sm-9">` + workPackage.wbsNum + `</dd>
+                        <dt class="col-sm-3">WP Name</dt>
+                        <dd class="col-sm-9">` + workPackage.name + `</dd>
+                        <dt class="col-sm-3">Duration</dt>
+                        <dd class="col-sm-9">` + workPackage.duration + `</dd>
+                        <dt class="col-sm-3">Budget</dt>
+                        <dd class="col-sm-9">` + workPackage.budget + `</dd>
+                        <dt class="col-sm-3">Dependancies</dt>
+                        <dd class="col-sm-9">` + workPackage.dependancies + `</dd>
+                        <dt class="col-sm-3">Deliverable(s)</dt>
+                        <dd class="col-sm-9">` + workPackage.deliverable + `</dd>
+                        <dt class="col-sm-3">Description</dt>
+                        <dd class="col-sm-9">` + workPackage.description + `</dd>
+                        <dt class="col-sm-3">Changes</dt>
+                        <dd class="col-sm-9">` + workPackage.changes + `</dd>
+                    </dl>
+                </div>`;
+    return html;
+}
