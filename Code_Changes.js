07e65f9c78b79bf2a47ea62c15c7d09391a8c0f8
James C-D <james.changdavidson@gmail.com>
2020-07-21 11:24:45 -0700

    added status update submit feature, no database editing

--- Code_Changes.js
+++ Code_Changes.js
@@ -20,10 +20,89 @@ function getChangeForm(changeType) {
 function getStatusUpdateForm() {
     var html = `<div class="data-frame">
                     <h4>Status Update</h4>
-                    ` + getWBSFormGroup("status") + `
-                    <div id="wp-content">
+                    <div class="secondary-input">
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">WBS #</span>
+                            </div>
+                            <input type="text" class="form-control" placeholder="X.X.X.X.X" 
+                                    aria-label="Work Breakdown Structure Number"id="status-go-input">
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" type="submit" id="status-go-btn">Go</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="status-content">
                         ` + getPlaceholderHTML("* Coming Soon! * <br> Enter WBS # Above") + `
                     </div>
                 </div>`;
+    return html;
+}
+
+// getChangeRequestForm : n/a -> HTML
+// return HTML display output for change request google form
+function getChangeRequestForm() {
+    var html = `<div class="change-form">
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdofsnmBQJFXOpPMG7-WPGrgDAASAlu4EMryaH8oEDw9mPMDA/viewform?embedded=true" 
+                            width="640" height="1500" frameborder="0" marginheight="0" marginwidth="0">
+                        Loading…
+                    </iframe>
+                </div>`;
+    return html;
+}
+
+// getStatusUpdateContent : String -> HTML
+// Produce HTML display output for given WBS #'s work package
+function getStatusUpdateContent(wbsNum) {
+    var workPackage = getWorkPackageObj(wbsNum);
+    var html = `<div class="data-frame">
+                    <dl class="row">
+                        <dt class="col-sm-3">Project</dt>
+                        <dd class="col-sm-9">` + workPackage.project + `</dd>
+                        <dt class="col-sm-3">Project Lead</dt>
+                        <dd class="col-sm-9">` + workPackage.lead + `</dd>
+                        <hr>
+                        <dt class="col-sm-3">WBS #</dt>
+                        <dd class="col-sm-9">` + workPackage.wbsNum + `</dd>
+                        <dt class="col-sm-3">WP Name</dt>
+                        <dd class="col-sm-9">` + workPackage.name + `</dd>
+                        <hr>
+                        <dt class="col-sm-3">Current Status</dt>
+                        <dd class="col-sm-9">` + "50% [Placeholder]" + `</dd>
+                        <dt class="col-sm-3">New Status</dt>
+                        <dd class="col-sm-9">` + getNewStatusInput() + `</dd>
+                    </dl>
+                </div>`;
+    return html;
+}
+
+// getNewStatusInput : n/a -> HTML
+// Produce HTML display output for selecting new status
+function getNewStatusInput() {
+    var html = `<div class="input-group mb-3">
+                    <select class="custom-select" id="new-status-go-input" aria-label="Status select with go button">
+                        <option selected>Choose...</option>
+                        <option value="0.00">0%</option>
+                        <option value="0.25">25%</option>
+                        <option value="0.50">50%</option>
+                        <option value="0.75">75%</option>
+                        <option value="1.00">100%</option>
+                    </select>
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="submit" id="new-status-go-btn">Submit</button>
+                    </div>
+                </div>`;
+    return html;
+}
+
+// setNewStatus : Object[New Status] -> HTML
+// Sets status of WBS # in newStatus to number in newStatus, return HTML for success message
+function setNewStatus(newStatus) {
+    var html = `<div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>Success!</strong> Set ` + newStatus.wbs + ` to ` + newStatus.status + ` [TEST]
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>`;
     return html;
 }
