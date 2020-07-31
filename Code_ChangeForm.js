2e437b3f8e07a3e6d59a0bd762e364937b4f61a9
James C-D <james.changdavidson@gmail.com>
2020-07-30 17:34:01 -0700

    added connection to change request form, just console logs submissions

---
+++ Code_ChangeForm.js
@@ -0,0 +1,30 @@
+/*
+Document: JS code connecting the Change Management Request form to the spreadsheet
+
+*/
+
+// onRequestSubmit : n/a -> n/a
+// Pull the new change request submitted to the form
+function onRequestSubmit() {
+    var form = FormApp.openById(scriptProps.getProperty('changeForm'));
+    var responses = form.getResponses();
+    var latestResponse = responses[responses.length - 1];
+    var responseData = latestResponse.getItemResponses();
+    for (var j = 0; j < responseData.length; j++) {
+        var item = responseData[j];
+        Logger.log('Response #%s to the question "%s" was "%s"', 
+                    (j + 1).toString(), 
+                    item.getItem().getTitle(),
+                    item.getResponse());
+    }
+    var data = {
+        idNum: "",
+        dateTime: "",
+        requestor: "",
+        wbsNum: "",
+        what: "",
+        why: "",
+        impact: "",
+        docs: "",
+    }
+}
