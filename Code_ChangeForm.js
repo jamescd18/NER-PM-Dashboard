a7285738eed606c98a9eb2842c97c33c6ddd206f
jamescd18 <51571627+jamescd18@users.noreply.github.com>
2020-08-02 01:25:10 -0700

    expanded functionality of link to change request form feature

--- Code_ChangeForm.js
+++ Code_ChangeForm.js
@@ -32,5 +32,38 @@ function onRequestSubmit() {
 // getNextChangeRequestId : String -> String
 // returns the next request ID for the given request type based on previous requests
 function getNextChangeRequestId(requestType) {
-    console.log(requestType);
+    var typeLetter;
+    if (requestType == "New Function") {
+        typeLetter = "N";
+    } else if (requestType == "Design Issue") {
+        typeLetter = "I";
+    } else if (requestType == "Project Delay") {
+        typeLetter = "D";
+    } else if (requestType == "Budget") {
+        typeLetter = "B";
+    } else if (requestType == "Stage Transition") {
+        typeLetter = "S";
+    } else if (requestType == "Other") {
+        typeLetter = "O";
+    } else {
+        throw "Request type not supported, found " + requestType;
+    }
+    var data = getSheetInfo('mainSheetID', 'Change Requests', 'data');
+    var changeIdIdx = findIdx("Request ID", data[0]);
+    var numRequests = data.length;
+    var latestId;
+    for (var rowIdx = numRequests; rowIdx > 0; rowIdx--) {
+        var rowId = data[rowIdx][changeIdIdx];
+        if (rowId.includes(typeLetter)) {
+            latestId = rowId;
+            break;
+        }
+    }
+    if (latestId == null) {
+        return typeLetter + "-001";
+    } else {
+        var numStr = "0." + latestId.substring(latestId.indexOf("-") + 1); // converts ID (N-001) into number string ("0.001")
+        var nextNum = (parseFloat(numStr) + 0.001).toString(); // converts string to float and increments by 1
+        return typeLetter + "-" + nextNum.substring(nextNum.indexOf(".") + 1); // converts back to string ID form
+    }
 }
--- Code_Changes.js
+++ Code_Changes.js
@@ -110,6 +110,6 @@ function setNewStatus(newStatus) {
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
-                    </div>`;
+                </div>`;
     return html;
 }
