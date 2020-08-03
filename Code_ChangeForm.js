a74af12c45e51f6d48fd80eb111829f3d9104fe6
James C-D <james.changdavidson@gmail.com>
2020-08-02 18:41:07 -0700

    cleaned up link to change request form

--- Code_ChangeForm.js
+++ Code_ChangeForm.js
@@ -10,39 +10,17 @@ function onRequestSubmit() {
     var responses = form.getResponses();
     var latestResponse = responses[responses.length - 1];
     var responseData = latestResponse.getItemResponses();
-    var data = [
-        getNextChangeRequestId(responseData[2].getResponse()), // id num
-        latestResponse.getTimestamp(), // date time
-        responseData[0].getResponse(), // requestor
-        responseData[1].getResponse(), // wbs number
-        responseData[3].getResponse(), // what
-        responseData[4].getResponse(), // why
-        responseData[5].getResponse(), // impact
-        responseData[6].getResponse(), // docs
-    ]
+    responseData = responseData.map(ele => ele.getResponse()); // convert items to item responses
+    var requestId = getNextChangeRequestId(responseData.splice(2, 1)[0]); // convert request type to id num
+    responseData.unshift(requestId, latestResponse.getTimestamp()); // add id num and timestamp to front of array
     var sheet = getSheetInfo('mainSheetID', 'Change Requests', 'sheet');
-    sheet.appendRow(data);
+    sheet.appendRow(responseData);
 }
 
 // getNextChangeRequestId : String -> String
 // returns the next request ID for the given request type based on previous requests
 function getNextChangeRequestId(requestType) {
-    var typeLetter;
-    if (requestType == "New Function") {
-        typeLetter = "N";
-    } else if (requestType == "Design Issue") {
-        typeLetter = "I";
-    } else if (requestType == "Project Delay") {
-        typeLetter = "D";
-    } else if (requestType == "Budget") {
-        typeLetter = "B";
-    } else if (requestType == "Stage Transition") {
-        typeLetter = "S";
-    } else if (requestType == "Other") {
-        typeLetter = "O";
-    } else {
-        throw "Request type not supported, found " + requestType;
-    }
+    var typeLetter = convertToTypeLetter(requestType);
     var data = getSheetInfo('mainSheetID', 'Change Requests', 'data');
     var changeIdIdx = findIdx("Request ID", data[0]);
     var numRequests = data.length - 1;
@@ -60,5 +38,25 @@ function getNextChangeRequestId(requestType) {
         var numStr = "0." + latestId.substring(latestId.indexOf("-") + 1); // converts ID (N-001) into number string ("0.001")
         var nextNum = (parseFloat(numStr) + 0.001).toString(); // converts string to float and increments by 1
         return typeLetter + "-" + nextNum.substring(nextNum.indexOf(".") + 1); // converts back to string ID form
+    }
+}
+
+// convertToTypeLetter : String -> String
+// convert long-form string to single character short-form of change request type
+function convertToTypeLetter(requestType) {
+    if (requestType == "New Function") {
+        return "N";
+    } else if (requestType == "Design Issue") {
+        return "I";
+    } else if (requestType == "Project Delay") {
+        return "D";
+    } else if (requestType == "Budget") {
+        return "B";
+    } else if (requestType == "Stage Transition") {
+        return "S";
+    } else if (requestType == "Other") {
+        return "O";
+    } else {
+        throw "Request type not supported, found " + requestType;
     }
 }
