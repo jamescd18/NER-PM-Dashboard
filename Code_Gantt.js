e22324cdc19977d4a5463473f5498e40a99bccdc
James C-D <james.changdavidson@gmail.com>
2020-08-03 18:10:39 -0700

    fixed iframe urls

--- Code_Gantt.js
+++ Code_Gantt.js
@@ -4,9 +4,7 @@
 // return HTML iframe display for the Gantt in the Database spreadsheet
 function getEntireGanttSheet() {
     var html = `<div class="big-gantt">
-                    <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ21BbKSgpjzx-GgFu8OymjbgaaWcp-
-                                    VnTcNdeFYiMmcib_LTpYQcs4229ZvGBwUNrB8zBpOqzYvF7v/pubhtml?gid=100811517
-                                    &amp;single=true&amp;widget=true&amp;headers=false">
+                    <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ21BbKSgpjzx-GgFu8OymjbgaaWcp-VnTcNdeFYiMmcib_LTpYQcs4229ZvGBwUNrB8zBpOqzYvF7v/pubhtml?gid=100811517&amp;single=true&amp;widget=true&amp;headers=false">
                     </iframe>
                 </div>`;
     return html;
