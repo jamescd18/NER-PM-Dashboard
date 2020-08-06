80ce6e1e2b5021c3aa76fc102530fd4b1389f77d
James C-D <james.changdavidson@gmail.com>
2020-08-06 15:57:11 -0700

    fixed sizing and styling for data boxes

--- Code_Gantt.js
+++ Code_Gantt.js
@@ -4,7 +4,8 @@
 // return HTML iframe display for the Gantt in the Database spreadsheet
 function getEntireGanttSheet() {
     var html = `<div class="big-gantt">
-                    <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ21BbKSgpjzx-GgFu8OymjbgaaWcp-VnTcNdeFYiMmcib_LTpYQcs4229ZvGBwUNrB8zBpOqzYvF7v/pubhtml?gid=100811517&amp;single=true&amp;widget=true&amp;headers=false">
+                    <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ21BbKSgpjzx-GgFu8OymjbgaaWcp-VnTcNdeFYiMmcib_LTpYQcs4229ZvGBwUNrB8zBpOqzYvF7v/pubhtml?gid=100811517&amp;single=true&amp;widget=true&amp;headers=false"
+                            style="width: 100%; height: 700px">
                     </iframe>
                 </div>`;
     return html;
